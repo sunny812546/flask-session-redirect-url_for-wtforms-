ef8233b8ccf8f943696a85d99756f8eb879724a9
sunny812546 <48019020+sunny812546@users.noreply.github.com>
2019-04-25 18:39:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import (StringField, BooleanField, validators,
                      RadioField, TextAreaField, SubmitField)
@@ -9,11 +9,8 @@ app.config["SECRET_KEY"] = "mykey"
 
 
 class Form(FlaskForm):
-    email = StringField("Enter Email", [validators.DataRequired()])
-    feedback = TextAreaField("Write Somethings")
-    gender = RadioField("Gender:", choices=[("male", "male"),
-                                            ("female", "female")])
-    age = BooleanField("are you 18+?")
+    name = StringField("Enter Name", [validators.DataRequired()])
+
     submit = SubmitField("submit")
 
 
@@ -21,11 +18,10 @@ class Form(FlaskForm):
 def home():
     form = Form()
     if form.validate_on_submit():
-        session["email"] = form.email.data
-        session["feedback"] = form.feedback.data
-        session["gender"] = form.gender.data
-        session["age"] = form.age.data
-        return redirect(url_for("thankyou"))
+        session["name"] = form.name.data
+        flash("thank you! {}".format(session["name"]))
+
+        return redirect(url_for("home"))
     return render_template("home.html", form=form)
 
 
